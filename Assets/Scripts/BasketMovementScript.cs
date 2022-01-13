55ae0497ceaad4fb6c9b0e488117b838bcb1d55d
21-BryanTan <pandapanda2266@hotmail.com>
2022-01-13 10:46:43 +0800

    Working UI Score Text

--- Assets/Scripts/BasketMovementScript.cs
+++ Assets/Scripts/BasketMovementScript.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BasketMovementScript : MonoBehaviour
 {
+    public GameObject ScoreText;
+
     public float speed;
 
     private int score;
@@ -11,7 +14,7 @@ public class BasketMovementScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        ScoreText.GetComponent<Text>().text = "Score:" + score;
     }
 
     // Update is called once per frame
@@ -21,6 +24,8 @@ public class BasketMovementScript : MonoBehaviour
       float horizontalInput = Input.GetAxis("Horizontal");
 
       transform.position = transform.position + new Vector3(horizontalInput * speed * Time.deltaTime, 0, 0);
+
+      ScoreText.GetComponent<Text>().text = "Score:" + score;
     }
 
     private void OnCollisionEnter(Collision other)
