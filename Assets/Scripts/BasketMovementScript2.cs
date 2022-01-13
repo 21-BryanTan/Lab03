c329c6bc57304ea3f5938af3b6063c655e470cb2
21-BryanTan <pandapanda2266@hotmail.com>
2022-01-13 11:58:35 +0800

    Added working Timer for Level 2

---
+++ Assets/Scripts/BasketMovementScript2.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class BasketMovementScript2 : MonoBehaviour
+{
+    public GameObject ScoreText;
+
+    public GameObject TimerText;
+
+    public float speed;
+
+    private int score;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        ScoreText.GetComponent<Text>().text = "Score: " + score;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+      float horizontalInput = Input.GetAxis("Horizontal");
+
+      transform.position = transform.position + new Vector3(horizontalInput * speed * Time.deltaTime, 0, 0);
+
+      ScoreText.GetComponent<Text>().text = "Score: " + score;
+
+        if(score >= 100)
+        {
+            SceneManager.LoadScene("GameWinScene");
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.CompareTag("Healthy"))
+        {
+            Destroy(other.gameObject);
+            score += 10;
+            print("Healthy food collected!");
+        }
+        else if(other.gameObject.CompareTag("Unhealthy"))
+        {
+            Destroy(other.gameObject);
+            SceneManager.LoadScene("GameLoseScene");
+            print("Unhealthy food collected!");
+        }
+    }
+}
--- Assets/Scripts/TimerScript.cs
+++ Assets/Scripts/TimerScript.cs
@@ -1,18 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimerScript : MonoBehaviour
 {
+
+    public float currentTime = 0f;
+    public float startingTime = 10f;
+
+    [SerializeField] Text countdownText;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentTime = startingTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        currentTime -= Time.deltaTime;
+        countdownText.text = currentTime.ToString("Timer: " + "0" + " Sec");
+
+        if(currentTime <= 0)
+        {
+            currentTime = 0;
+        }
     }
 }
